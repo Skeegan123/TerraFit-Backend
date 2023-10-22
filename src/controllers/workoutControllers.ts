6e5446592efb2a0bf02ff7bf0336b3c8df1cb247
Skeegan123 <mcfeegan123@gmail.com>
2023-10-22 07:37:39 -0400

    Added time question

--- src/controllers/workoutControllers.ts
+++ src/controllers/workoutControllers.ts
@@ -5,10 +5,11 @@ import OpenAI from "openai";
 
 export const createWorkout = async (req: Request, res: Response) => {
 
-    const { level, focus, user_id } = req.body;
+    const { time, level, focus, user_id } = req.body;
 
     console.log("Level: " + level);
     console.log("Focus: " + focus);
+    console.log("Time: " + time);
 
     // ChatGPT API implementation
 
@@ -18,8 +19,8 @@ export const createWorkout = async (req: Request, res: Response) => {
         const completion: any = await openai.chat.completions.create({
             messages: [
                 // { role: "system", content: "You are an AI program designed to create workout plans for people based on a variety of factors. You will receive a message in the form of \"LEVEL: BEGINNER; FOCUS: EVERYTHING;\" And respond with an array of 3 workout plans of different lengths of time. These workout plans will be in pure JSON and each object in the array will follow the format of the following JSON: { workoutTitle: \"Creative title for a workout\", estimatedTime: \"45 minutes\" requiredEquipment: [ \"Bench Press\", \"Dumbells\", \"Squat Rack\", ], plan: [ { title: \"Bicep Curls\", sets: 3, reps: 10, equipment: \"Dumbells\", notes: \"\", }, { title: \"Bench Press\", sets: 3, reps: 5, equipment: \"Bench Press\", notes: \"\", }, { title: \"Plank\", time: \"1 minute\" equipment: \"None\", notes: \"\", }, ] } You will only choose exercises from the following list of exercises as these are the only ones we support at this time: Squats, Deadlifts, Bench Press, Push-ups, Planks, Overhead Press, Pull-ups, Lunges, Bent-over Rows, Dumbbell Curls, Tricep Extensions, Burpees, Leg Press, Mountain Climbers, Lat Pulldowns, Seated Rows, Jumping Jacks, Sit-ups, Bicycle Crunches, Kettlebell Swings, Romanian Deadlifts, Chin-ups, Dumbbell Flyes, Calf Raises, Skull Crushers, Russian Twists, Box Jumps" },
-                { role: "system", content: "You are an AI program designed to create workout plans for people based on a variety of factors. You will receive a message in the form of \"user_id: 1; LEVEL: BEGINNER; FOCUS: EVERYTHING;\" And respond with pure JSON in the form of: { user_id: 1, workoutTitle: \"Creative title for a workout\", estimatedTime: \"45 minutes\" requiredEquipment: [ \"Bench Press\", \"Dumbells\", \"Squat Rack\", ], plan: [ { title: \"Bicep Curls\", sets: 3, reps: 10, equipment: \"Dumbells\", notes: \"\", }, { title: \"Bench Press\", sets: 3, reps: 5, equipment: \"Bench Press\", notes: \"\", }, { title: \"Plank\", time: \"1 minute\" equipment: \"None\", notes: \"\", }, ] } You will only choose exercises from the following list of exercises as these are the only ones we support at this time: Squats, Deadlifts, Bench Press, Push-ups, Planks, Overhead Press, Pull-ups, Lunges, Bent-over Rows, Dumbbell Curls, Tricep Extensions, Burpees, Leg Press, Mountain Climbers, Lat Pulldowns, Seated Rows, Jumping Jacks, Sit-ups, Bicycle Crunches, Kettlebell Swings, Romanian Deadlifts, Chin-ups, Dumbbell Flyes, Calf Raises, Skull Crushers, Russian Twists, Box Jumps" },
-                { role: "user", content: `user_id: ${user_id}, LEVEL: ${level}; FOCUS: ${focus};` }],
+                { role: "system", content: "You are an AI program designed to create workout plans for people based on a variety of factors. You will receive a message in the form of \"user_id: 1; LEVEL: BEGINNER; FOCUS: EVERYTHING; TIME-CONSTRAINT: 30 MINUTES\" And respond with pure JSON in the form of: { user_id: 1, workoutTitle: \"Creative title for a workout\", estimatedTime: \"30 minutes\" requiredEquipment: [ \"Bench Press\", \"Dumbells\", \"Squat Rack\", ], plan: [ { title: \"Bicep Curls\", sets: 3, reps: 10, equipment: \"Dumbells\", notes: \"\", }, { title: \"Bench Press\", sets: 3, reps: 5, equipment: \"Bench Press\", notes: \"\", }, { title: \"Plank\", time: \"1 minute\" equipment: \"None\", notes: \"\", }, ] } Pay close attention to the time constraint as this is a hard limit and you must not make a workout that takes longer than this limit. However you should still try and give an accurate time estimate in minutes or hours. You will only choose exercises from the following list of exercises as these are the only ones we support at this time: Squats, Deadlifts, Bench Press, Push-ups, Planks, Overhead Press, Pull-ups, Lunges, Bent-over Rows, Dumbbell Curls, Tricep Extensions, Burpees, Leg Press, Mountain Climbers, Lat Pulldowns, Seated Rows, Jumping Jacks, Sit-ups, Bicycle Crunches, Kettlebell Swings, Romanian Deadlifts, Chin-ups, Dumbbell Flyes, Calf Raises, Skull Crushers, Russian Twists, Box Jumps" },
+                { role: "user", content: `user_id: ${user_id}, LEVEL: ${level}; FOCUS: ${focus}; TIME-CONSTRAINT: ${time};` }],
             model: "gpt-4",
         });
 
